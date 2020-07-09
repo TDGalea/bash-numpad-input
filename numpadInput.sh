6c88e36ab7d0421740a5c5a09d522a642668a24d
GoDzM4TT3O <saloniamatteo@pm.me>
2020-07-09 17:50:58 +0200

    Greatly improve script
    1. Converted all "echo" commands to "printf".
    2. Use "[[ CONDITION ]] && doSomething" instead of "if [ CONDITION ];then doSomething"
    3. Use Regex to check if input is number ($inpIsNum)
    4. Check if folder "numpadInput" exists in current directory, so it won't give an error if it does not
    5. Fix syntax (from "while true; do" to "while true (NEWLINE) do"
    
    Please check if this works before merging into master branch, as I currently can't use a computer. (I wrote this using Termux on android, it does not have tput)

--- numpadInput.sh
+++ numpadInput.sh
@@ -8,134 +8,80 @@
 
 tput civis
 
-# Script expects a folder "numpadInput" in the same location as itself.
-cd numpadInput
+[[ -d ./numpadInput ]] && cd numpadInput || printf "Warning: numpadInput directory is missing!\n"
 scriptRoot="$PWD"
 # We use a variable to contain the "beep" command so that it can be disabled by the user.
 beep="beep"
 
 # Main loop.
-while true;do
+while true
+do
 	clear
 	# Print date and time at top of screen.
-	echo -e " \e[30;47m "`date +%H:%M`" \e[0m \e[30;47m "`date "+%A, %d %B %Y"`" \e[0m \e[30;47m $HOSTNAME \e[0m "
-	echo;echo
+	printf "\e[30;47m `date +%H:%M` \e[0m \e[30;47m \"`date \"+%A, %d %B %Y\"`\" \e[0m \e[30;47m $HOSTNAME \e[0m \n\n\n"
 
 	# List options in current folder, assigning a number to each.
 	# Colour dependant on whether item is a folder or file.
 	x=0
-	for item in *;do
-		echo -ne " \e[30;47m $x \e[0m "
+	for item in *
+	do
+		printf " \e[30;47m $x \e[0m "
 		# Check if folder or file.
-		if [ -d "$item" ];then
-			cutdown="${item##*_}"
-			echo -e "\e[34m$cutdown\e[0m"
-			unset cutdown
-			#echo -e "\e[34m$item\e[0m"
-		else
-			cutdown="${item##*_}"
-			cutdown="${cutdown%.*}"
-			echo -e "\e[32m$cutdown\e[0m"
-			unset cutdown
-			#echo -e "\e[32m${item%.*}\e[0m"
-		fi
+		[[ -d "$item" ]] && cutdown="${item##*_}" && printf "\e[34m$cutdown\e[0m\n" && \
+		#printf "\e[34m$item\e[0m\n"
+		unset cutdown || cutdown="${item##*_}" && cutdown="${cutdown%.*}" && printf "\e[32m$cutdown\e[0m\n" && \
+		#printf "\e[32m${item%.*}\e[0m\n"
+		unset cutdown
 		let x+=1
-		echo
+		printf "\n"
 	done
 
 	# Send cursor to bottom of page and print controls help.
 	tput cup $(expr $(tput lines) - 3)
-	echo -ne " \e[30;47m - \e[0m Switch to SRV0   "
-	echo;echo
-	echo -ne " \e[30;47m 0-9 \e[0m Menu Item   "
-	echo -ne " \e[30;47m / \e[0m Main Menu   "
-	echo -ne " \e[30;47m . \e[0m Back   "
-	echo -ne " \e[30;47m + \e[0m Toggle Beeps   "
-	echo -ne " \e[30;47m * \e[0m Restart Menu   "
+	printf " \e[30;47m - \e[0m Switch to SRV0   \n\n"
+	printf " \e[30;47m 0-9 \e[0m Menu Item   "
+	printf " \e[30;47m / \e[0m Main Menu   "
+	printf " \e[30;47m . \e[0m Back   "
+	printf " \e[30;47m + \e[0m Toggle Beeps   "
+	printf " \e[30;47m * \e[0m Restart Menu   "
 
 	# Set cursor colour to black so that the user input doens't appear on-screen.
-	echo -ne "\e[30m"
+	printf "\e[30m"
 	# Take input.
 	inpIsNumber=0
 	inp=""
 	read -t10 -n1 inp
 	# Reset cursor colour.
-	echo -ne "\e[0m"
-	# Check if input is number (I'm pretty sure there's a prettier way to do this but oh well).
-	case $inp in
-    	0) inpIsNumber=1;;
-	    1) inpIsNumber=1;;
-    	2) inpIsNumber=1;;
-	    3) inpIsNumber=1;;
-    	4) inpIsNumber=1;;
-	    5) inpIsNumber=1;;
-    	6) inpIsNumber=1;;
-	    7) inpIsNumber=1;;
-    	8) inpIsNumber=1;;
-	    9) inpIsNumber=1;;
-	esac
-	if [ "$inpIsNumber" = "1" ];then
+	printf "\e[0m"
+	# Check if input is number
+	[[ $yournumber =~ '^[0-9]+$' ]] && inpIsNum = 0 || inpIsNum = 1
+	[[ $inpIsNum == "0" ]] &&
 		# Input is number. Cycle through items until number reached.
 		x=0
 		target=""
-		for currentItem in *;do
-			if [ "$x" = "$inp" ];then
-				target=$currentItem
-			fi
+		for currentItem in *
+		do
+			[[ "$x" = "$inp" ]] && target=$currentItem
 			let x+=1
 		done
 		# Check if item exists, and if file or folder.
-		if [ -d "$target" ];then
-			$beep -f1000 -l50 &
-			cd "$target"
-		elif [ -x "$target" ];then
-			$beep -f1000 -l50 -nf1200 -l50 &
-			./"$target"
-		else
-			$beep -f600 -l50 &
-		fi
-	else
+		[[ -d "$target" ]] && $beep -f1000 -l50 & cd "$target" || [[ -x "$target" ]] && $beep -f1000 -l50 -nf1200 -l50 & ./"$target" || $beep -f600 -l50 &
+	[[ $inpIsNum == "1" ]] &&
 		# Input not a number. Check if it's one of the in-script functions.
+		[[ "$inp" = "/" ]] && \
 		# Return to root.
-		if [ "$inp" = "/" ];then
-			$beep -f1000 -l50 -nf750 -l50 &
-			cd $scriptRoot
-		# Parent folder.
-		elif [ "$inp" = "." ];then
-			$beep -f750 -l50
-			if [ "$PWD" != "$scriptRoot" ];then
-				cd ..
-			fi
-		# Toggle beeping.
-		elif [ "$inp" = "+" ];then
-			if [ "$beep" = "beep" ];then
-				# Set command in '$beep' to 'true', which simply exits with an exit code of 0.
-				beep="true"
-				beep -f1000 -l50 -nf750 -l50 &
-			else
-				# Set command in '$beep' to 'beep'.
-				beep="beep"
-				beep -f750 -l50 -nf1000 -l50 &
-			fi
-		# Connect to SRV0.
-		elif [ "$inp" = "-" ];then
-			$beep -f750 -l50 -nf1000 -l50 -nf1200 -l50
-			ssh srv0
-		# Exit script.
-		elif [ "$inp" = "*" ];then
-			$beep -f1000 -l50 -nf750 -l50 -nf1000 -l50
-			exit 0
-		# Empty input - Either timeout, space or enter.
-		elif [ "$inp" = "" ];then
-			true
-		else
-			# No matches.
-			$beep -f600 -l50 &
-		fi
-	fi
+		$beep -f1000 -l50 -nf750 -l50 & cd $scriptRoot || [[ "$inp" = "." ]] && \
+		# Parent folder
+		$beep -f750 -l50 && [[ "$PWD" != "$scriptRoot" ]] && cd .. || [[ "$inp" = "+" ]] && \
+		# Toggle beeping
+		[[ "$beep" = "beep" ]] && beep="true" && beep -f1000 -l50 -nf750 -l50 || beep="beep" && beep -f750 -l50 -nf1000 -l50 || [[ "$inp" = "-" ]] && $beep -f750 -l50 -nf1000 -l50 -nf1200 -l50 && ssh srv0 || [[ "$inp" = "*" ]] && \
+		# Exit script
+		$beep -f1000 -l50 -nf750 -l50 -nf1000 -l50 && exit 0 || [[ "$inp" = "" ]] && \
+			# No match
+			true || $beep -f600 -l50 &
 done
 
-echo "Reached end of script. How?"
+printf "Reached end of script. How?"
 read -n1 pause
 unset pause
 exit 1
